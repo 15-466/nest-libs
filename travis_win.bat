31d29e2e9e992ad52942ee13363949390770775a
Jim McCann <ix@tchow.com>
2019-08-26 23:16:29 -0400

    trying it two ways

--- travis_win.bat
+++ travis_win.bat
@@ -8,5 +8,8 @@ refreshenv
 echo BEFORE vcvars
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 echo AFTER vcvars
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -test
+echo AFTER vsdevcmd
 C:\Python37\python rebuild-libs.py all package
 echo AFTER rebuild-libs
