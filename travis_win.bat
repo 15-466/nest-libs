f246ecf5edd82f70fd2e19638db5e7c07963f6af
Jim McCann <ix@tchow.com>
2019-08-26 22:48:42 -0400

    trying yet again

--- travis_win.bat
+++ travis_win.bat
@@ -3,5 +3,8 @@ choco install visualstudio2019-workload-vctools
 choco install python3
 choco install 7zip
 refreshenv
+@echo BEFORE vcvars
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+@echo AFTER vcvars
 C:\Python37\python rebuild-libs.py all package
+@echo AFTER rebuild-libs
