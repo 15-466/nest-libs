be1107cbae921d42f9add3379c20302d4eda92e9
Jim McCann <ix@tchow.com>
2019-08-26 22:20:05 -0400

    hmm, maybe this is where vcvars is

--- travis_win.bat
+++ travis_win.bat
@@ -3,5 +3,5 @@ choco install visualstudio2019-workload-vctools
 choco install python3
 choco install 7zip
 refreshenv
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 C:\Python37\python rebuild-libs.py all package
