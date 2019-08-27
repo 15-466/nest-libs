2921208677e0834b51a342e61a280e1bf54afdf0
Jim McCann <ix@tchow.com>
2019-08-27 00:26:35 -0400

    After many messy hacks, windows build seems to be properly updating its release.

--- travis_win.bat
+++ travis_win.bat
@@ -1,8 +1,2 @@
-echo BEFORE vcvars
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-echo AFTER vcvars
-rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
-rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -test
-rem echo AFTER vsdevcmd
 C:\Python37\python rebuild-libs.py all package
-echo AFTER rebuild-libs
