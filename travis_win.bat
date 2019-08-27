786b4927ed40ffd78b292a4b8413a133977abbfd
Jim McCann <ix@tchow.com>
2019-08-26 23:09:58 -0400

    yet more information

--- travis_win.bat
+++ travis_win.bat
@@ -2,6 +2,7 @@ choco install visualstudio2019buildtools
 choco install visualstudio2019-workload-vctools
 choco install python3
 choco install 7zip
+@echo BEFORE refreshenv
 refreshenv
 @echo BEFORE vcvars
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
