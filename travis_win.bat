454490e461705be3a80afc5361c91933229b2c82
Jim McCann <ix@tchow.com>
2019-08-26 23:13:18 -0400

    maybe at does the thing I think it does

--- travis_win.bat
+++ travis_win.bat
@@ -1,12 +1,12 @@
-@echo BEFORE choco install
+echo BEFORE choco install
 choco install visualstudio2019buildtools
 choco install visualstudio2019-workload-vctools
 choco install python3
 choco install 7zip
-@echo BEFORE refreshenv
+echo BEFORE refreshenv
 refreshenv
-@echo BEFORE vcvars
+echo BEFORE vcvars
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-@echo AFTER vcvars
+echo AFTER vcvars
 C:\Python37\python rebuild-libs.py all package
-@echo AFTER rebuild-libs
+echo AFTER rebuild-libs
