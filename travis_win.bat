82427daabb63cbc25722f17d51449c0fd9580dfc
Jim McCann <ix@tchow.com>
2019-08-26 23:49:29 -0400

    okay, refreshenv definitely seems to be a problem

--- travis_win.bat
+++ travis_win.bat
@@ -1,10 +1,3 @@
-echo BEFORE choco install
-choco install visualstudio2019buildtools
-choco install visualstudio2019-workload-vctools
-choco install python3
-choco install 7zip
-echo BEFORE refreshenv
-refreshenv
 echo BEFORE vcvars
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 echo AFTER vcvars
